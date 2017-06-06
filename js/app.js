ae4db9f66b48612b970052cb6a01a18667debd07
BlindNerd <blindnerd@Blinds-MacBook-Pro.local>
2017-06-06 10:36:14 -0400

    Search still isn't done.
    Getting errors at the text content

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const students = $('.student-item');
 const numberOfStudents = $('.student-item').length;
 const studentsPerPage = 10;
 let pageNumber = 0;
-const searchHTML = '<div class = "student-search"><input id = "search" class = "student-search" type = "search" placeholder = "Search Students"></div>';
+const searchHTML = '<div class = "student-search"><input id = "search" class = "student-search" type = "search" placeholder = "Search Students"><button id = "searchButton" class = "student-search">Search</button></div>';
 const searchButton = $('#searchButton');
 const ul = $('.student-list');
 const noResults = '<h1>No Students Found</h1>'
@@ -19,6 +19,7 @@ let studentEmail = $('.email');
 
 function showPage(pageNumber, students) {  // creating the showPage function and passing in pageNumber and student list
    $(students).hide();    // first hide all students on the page
+
     for (let i = 0; i < numberOfStudents; i++ ) {  // Then loop through all students in our student list
       if ( i < pageNumber * studentsPerPage && i > (pageNumber - 1) * studentsPerPage - 1  ) { // if student should be on this page number
           $(students[i]).show(333);   // show the student page over thrid a second.
@@ -29,10 +30,13 @@ function showPage(pageNumber, students) {  // creating the showPage function and
 function addLinks (students) {
   let numberOfPages = Math.ceil(students.length / 10);  // determine how many pages for this student list
   let pagination = '<div class = "pagination">'; // add div for links maiking it a class of pagination
+
   pagination += '<ul>'; // add to div
+
     for (let i = 1; i <= numberOfPages; i++) { // loop through all pages
       pagination += '<li><a href = "#">' + i + '</a></li>'; //creating page links and adding them to the div
     }
+
   pagination += '</ul></div>'; // add the rest of the HTML
   $(ul).after(pagination); // add it to the dom
 
@@ -42,34 +46,33 @@ function addLinks (students) {
     $('.pagination li a').removeClass('active'); // change classes when clicked
     $(this).addClass('active'); // add active to your clicked item
       showPage(pageNumber, students); // call the show page function to show the right students on the page
-       console.log(pageNumber);
 });
 }
 
                       // Search jquery function
-    $('#search').on('keyup', function() {  // grab the value as the user enters info
-            $('.pagination').hide();
-            let matchedStudents = [];
+    $('#searchButton').on('click', function() {  // grab the value as the user enters info
+            $('.pagination').hide(); // hide page links
+            let matchedStudents = []; // make an empty array to hold the matched students
             let search = $('#search').val().toLowerCase(); // grab the value of the search box an make it lower case
 
         for ( i = 0; i < numberOfStudents; i++ ) {
-            studentName = studentName[i].textContent.toLowerCase();
-            studentEmail = studentEmail[i].textContent.toLowerCase();
+            studentName = studentName[i].textContent; // loop through stduents Name
+            studentEmail = studentEmail[i].textContent;
 
-          if ( studentName.search(search) !== -1 || studentEmail.search(search) ) {
+          if ( studentName == search || studentEmail == search ) { // results of search
             matchedStudents.push(students[i]);
         }
       }
 
-            if ( matchedStudents.length === 0) {
+            if ( matchedStudents.length === 0) {  // if no matches found
               $(students).hide();
-              $('.student-list').after(noResults);
+              $('.student-list').after(noResults);  // show no results section
 
-              }else if ( matchedStudents.length < 10 ) {
-                showPage(1, matchedStudents);
+            }else if ( matchedStudents.length < 10 ) {  // if matched students length is more than 10
+                showPage(1, matchedStudents); // call showPage function passing matched students
 
                 }else {
-                  showPage(1, matchedStudents);
+                  showPage(1, matchedStudents); // call pagination functions on results
                   addLinks(matchedStudents);
                 }
 
