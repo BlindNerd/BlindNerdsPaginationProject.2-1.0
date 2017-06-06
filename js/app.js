8ff6e433d3e8b147a2a631b15b880ef8aa011d45
BlindNerd <blindnerd@Blinds-MacBook-Pro.local>
2017-06-06 09:02:19 -0400

    working on search function
    Still working on search decided to rewrite it I took the each() off and
    did a for loop instead following the guide more I should still add a
    button and add a click function and abandon the keyup.

--- js/app.js
+++ js/app.js
@@ -2,66 +2,78 @@
                   // creating variable
 
 
-const $students = $('.student-item');
-const $numberOfStudents = $('.student-item').length;
-const numberOfPages = Math.ceil($numberOfStudents / 10);  // determine how many pages for this student list
+const students = $('.student-item');
+const numberOfStudents = $('.student-item').length;
 const studentsPerPage = 10;
 let pageNumber = 0;
 const searchHTML = '<div class = "student-search"><input id = "search" class = "student-search" type = "search" placeholder = "Search Students"></div>';
 const searchButton = $('#searchButton');
-const $pagination = $('.pagination');
-const $ul = $('.student-list');
+const ul = $('.student-list');
 const noResults = '<h1>No Students Found</h1>'
-
+let studentName = $('h3');
+let studentEmail = $('.email');
 
   $('h2').after(searchHTML);  // adding the search HTML to the page
 
 
 
-function showPage(pageNumber, $students) {  // creating the showPage function and passing in pageNumber and student list
-   $($students).hide();    // first hide all students on the page
-    for (let i = 0; i < $numberOfStudents; i++ ) {  // Then loop through all students in our student list
+function showPage(pageNumber, students) {  // creating the showPage function and passing in pageNumber and student list
+   $(students).hide();    // first hide all students on the page
+    for (let i = 0; i < numberOfStudents; i++ ) {  // Then loop through all students in our student list
       if ( i < pageNumber * studentsPerPage && i > (pageNumber - 1) * studentsPerPage - 1  ) { // if student should be on this page number
-          $($students[i]).show(333);   // show the student page over thrid a second.
+          $(students[i]).show(333);   // show the student page over thrid a second.
       }
   }
 }
         // creating pagination
-function addLinks (numberOfPages) {
+function addLinks (students) {
+  let numberOfPages = Math.ceil(students.length / 10);  // determine how many pages for this student list
   let pagination = '<div class = "pagination">'; // add div for links maiking it a class of pagination
   pagination += '<ul>'; // add to div
     for (let i = 1; i <= numberOfPages; i++) { // loop through all pages
       pagination += '<li><a href = "#">' + i + '</a></li>'; //creating page links and adding them to the div
     }
   pagination += '</ul></div>'; // add the rest of the HTML
-  $($ul).after(pagination); // add it to the dom
+  $(ul).after(pagination); // add it to the dom
 
 
   $('a').click( function (event){
       pageNumber = event.target.text;
     $('.pagination li a').removeClass('active'); // change classes when clicked
     $(this).addClass('active'); // add active to your clicked item
-      showPage(pageNumber, $students); // call the show page function to show the right students on the page
+      showPage(pageNumber, students); // call the show page function to show the right students on the page
        console.log(pageNumber);
 });
 }
 
                       // Search jquery function
-    $("#search").on("keyup", function() {  // grab the value as the user enters info
-          $('.pagination').hide();
-          let search = $(this).val().toLowerCase(); // grab the value of the search box an make it lower case
+    $('#search').on('keyup', function() {  // grab the value as the user enters info
+            $('.pagination').hide();
+            let matchedStudents = [];
+            let search = $('#search').val().toLowerCase(); // grab the value of the search box an make it lower case
 
-        $($students).each(function() {  // go through EACH student list item
-            let students = $(this).text().toLowerCase();
-          if ( students.indexOf(search) !== -1 ) {
-              $(this).show(333);  // results of the search
-          }else {
-              $(this).hide(333);
-            }
-        });
+        for ( i = 0; i < numberOfStudents; i++ ) {
+            studentName = studentName[i].textContent.toLowerCase();
+            studentEmail = studentEmail[i].textContent.toLowerCase();
 
+          if ( studentName.search(search) !== -1 || studentEmail.search(search) ) {
+            matchedStudents.push(students[i]);
+        }
+      }
+
+            if ( matchedStudents.length === 0) {
+              $(students).hide();
+              $('.student-list').after(noResults);
+
+              }else if ( matchedStudents.length < 10 ) {
+                showPage(1, matchedStudents);
+
+                }else {
+                  showPage(1, matchedStudents);
+                  addLinks(matchedStudents);
+                }
 
 });
-      addLinks(numberOfPages);
-      showPage(1, $students); // call first page when the page loads
+      addLinks(students);
+      showPage(1, students); // call first page when the page loads
       $('.pagination li a:first').addClass('active'); // when the page loads add active to the 1st page
